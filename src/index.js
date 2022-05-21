57ca0f342cc91d5a0cd3c4e83259fc4986ab80ad
Victor Merino <victor.merinogarcia@gmail.com>
2022-05-21 22:53:23 +0200

    An actual hello world app, in React 18

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
-console.log('Hola mundo')
+import { createRoot } from 'react-dom/client'
+
+const greeting = <h1>Hola Mundo</h1>
+
+const divRoot = document.querySelector('#root')
+
+const root = createRoot(divRoot)
+root.render(greeting)
