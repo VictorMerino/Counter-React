4862b88f0714856beb94de221beaaf9a1ada9d28
Victor Merino <victor.merinogarcia@gmail.com>
2022-05-21 23:05:09 +0200

    Some styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import FirstApp from './FirstApp'
 
+import './index.css'
 const divRoot = document.querySelector('#root')
 
 const root = createRoot(divRoot)
