f9153e51a56f049d48e7c1a016190f17a87ac321
Victor Merino <victor.merinogarcia@gmail.com>
2022-05-22 18:29:25 +0200

    Add the actual test

--- src/tests/base/07-deses-arr.test.js
+++ src/tests/base/07-deses-arr.test.js
@@ -1,4 +1,8 @@
-import {} from ''
-describe('', () => {
-  test('', () => {})
+import { returnArray } from '../../base/07-deses-arr'
+
+describe('07-deses-arr', () => {
+  test('returnArray should return a string and a number', () => {
+    const expectedArray = ['ABC', 123]
+    expect(returnArray()).toStrictEqual(expectedArray)
+  })
 })
