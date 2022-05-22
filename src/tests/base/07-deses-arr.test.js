c685c26c275039779e78c257cb62406924230716
Victor Merino <victor.merinogarcia@gmail.com>
2022-05-22 18:34:32 +0200

    Destructuring the array we can make more concrete expectations

--- src/tests/base/07-deses-arr.test.js
+++ src/tests/base/07-deses-arr.test.js
@@ -3,6 +3,9 @@ import { returnArray } from '../../base/07-deses-arr'
 describe('07-deses-arr', () => {
   test('returnArray should return a string and a number', () => {
     const expectedArray = ['ABC', 123]
-    expect(returnArray()).toStrictEqual(expectedArray)
+    const [firstReturnedValue, secondReturnedValue] = returnArray()
+
+    expect(firstReturnedValue).toStrictEqual(expectedArray[0])
+    expect(secondReturnedValue).toStrictEqual(expectedArray[1])
   })
 })
