8635898797965964f991b9e8eafc80a0e859956f
Victor Merino <victor.merinogarcia@gmail.com>
2022-05-22 13:35:11 +0200

    Add a counter to state: use of the first hook, with state for the Counter component

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,13 +1,15 @@
+import { useState } from 'react'
 import PropTypes from 'prop-types'
 
 const CounterApp = ({ value }) => {
+  const [counter, setCounter] = useState(0)
   const handleAddOne = event => {
-    console.log('handleAddOne called', event)
+    setCounter(counter + 1)
   }
   return (
     <>
       <h1>CounterApp</h1>
-      <h2>{value}</h2>
+      <h2>{counter}</h2>
 
       <button onClick={handleAddOne}>+1</button>
     </>
