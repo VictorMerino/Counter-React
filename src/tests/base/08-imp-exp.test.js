4d2d076c882e2fdb37f55dd611e2e3eecf5cc2c0
Victor Merino <victor.merinogarcia@gmail.com>
2022-05-22 19:28:00 +0200

    heroData can be used by any test

--- src/tests/base/08-imp-exp.test.js
+++ src/tests/base/08-imp-exp.test.js
@@ -1,7 +1,9 @@
-import { getHeroById } from '../../base/08-imp-exp'
+import { getHeroById, getHerosByOwner } from '../../base/08-imp-exp'
 import { heroes } from '../../data/heroes'
 
 describe('08-imp-exp', () => {
+  const heroData = heroes
+
   test('getHeroeById should return a hero', () => {
     const expectedHero = {
       id: 2,
@@ -10,15 +12,17 @@ describe('08-imp-exp', () => {
     }
     const returnedHero = getHeroById(2)
 
-    const heroData = heroes
-
     expect(returnedHero).toStrictEqual(expectedHero)
   })
 
   test('getHeroeById should return undefined if no hero', () => {
     const returnedHero = getHeroById(12) // 12 is out of bounds
 
-    const heroData = heroes
+    expect(returnedHero).toBeUndefined()
+  })
+
+  test('getHeroesByOwner should return an array with DC heros', () => {
+    const returnedHero = getHerosByOwner('DC')
 
     expect(returnedHero).toBeUndefined()
   })
