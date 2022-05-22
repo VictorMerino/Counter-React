98dbba86d6370985b5987f3b05a974ad8c459b9c
Victor Merino <victor.merinogarcia@gmail.com>
2022-05-22 13:43:51 +0200

    Add substract and reset handlers

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -7,12 +7,20 @@ const CounterApp = ({ value }) => {
     setCounter(counter + 1)
     // setCounter(counter => counter + 1) // This is the same as the previous one
   }
+  const handleSubstractOne = () => {
+    setCounter(counter - 1)
+  }
+  const handleReset = () => {
+    setCounter(value)
+  }
   return (
     <>
       <h1>CounterApp</h1>
       <h2>{counter}</h2>
 
-      <button onClick={handleAddOne}>+1</button>
+      <button onClick={handleSubstractOne}>- 1</button>
+      <button onClick={handleReset}>Reset</button>
+      <button onClick={handleAddOne}>+ 1</button>
     </>
   )
 }
