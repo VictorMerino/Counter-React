93ce140f1cce239a998f2286dbef8f211525c248
Victor Merino <victor.merinogarcia@gmail.com>
2022-05-22 20:30:21 +0200

    Add test to check for not found hero

--- src/tests/base/09-promises.test.js
+++ src/tests/base/09-promises.test.js
@@ -2,12 +2,22 @@ import { getHeroByIdAsync } from '../../base/09-promises'
 import { heros } from '../../data/heros'
 
 describe('09-promises', () => {
-  test('getHeroByIdAsync shoud return a hero', done => {
+  test('getHeroByIdAsync should return a hero', done => {
     const id = 1
 
     getHeroByIdAsync(id).then(hero => {
       expect(hero).toBe(heros[0])
       done()
     })
+  })
+
+  test('getHeroByIdAsync should return an error if id does not exists', done => {
+    const id = 13
+
+    getHeroByIdAsync(id).catch(error => {
+      expect(error).toContain('not found')
+      done()
+    })
+    // expect(getHeroByIdAsync(id)).toThrowError()
   })
 })
