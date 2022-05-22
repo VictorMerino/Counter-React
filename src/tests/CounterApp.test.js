eb3feb78b78a4f2be67a732622b164c115d14905
Victor Merino <victor.merinogarcia@gmail.com>
2022-05-22 22:36:06 +0200

    Add the full test suite: add, substract and reset methods tested like an user can do

--- src/tests/CounterApp.test.js
+++ src/tests/CounterApp.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import CounterApp from '../CounterApp'
 
 /**
@@ -6,9 +6,40 @@ import CounterApp from '../CounterApp'
  * show default value
  * substract
  * add
- * rese
+ * reset
  */
-describe.skip('CounterApp', () => {
+
+describe('CounterApp', () => {
   const defaultValue = 0
-  test('should show message "Hi Apple"', async () => {})
+  test('should show default value', () => {
+    render(<CounterApp value={defaultValue} />)
+    expect(Number(screen.getByTestId('result').textContent)).toBe(defaultValue)
+  })
+  test('should substract', async () => {
+    render(<CounterApp value={defaultValue} />)
+    const substractBtn = await screen.findByTestId('substract')
+    fireEvent.click(substractBtn)
+    expect(Number(screen.getByTestId('result').textContent)).toBe(-1)
+  })
+  test('should add', async () => {
+    render(<CounterApp value={defaultValue} />)
+    const addBtn = await screen.findByTestId('add')
+    fireEvent.click(addBtn)
+    expect(Number(screen.getByTestId('result').textContent)).toBe(1)
+  })
+  test.only('reset to default value', async () => {
+    const clickedTimes = 5
+    render(<CounterApp value={defaultValue} />)
+    const addBtn = await screen.findByTestId('add')
+
+    for (let i = 0; i < clickedTimes; i++) {
+      fireEvent.click(addBtn)
+    }
+    expect(Number(screen.getByTestId('result').textContent)).toBe(clickedTimes)
+
+    const resetBtn = await screen.findByTestId('reset')
+    fireEvent.click(resetBtn)
+
+    expect(Number(screen.getByTestId('result').textContent)).toBe(defaultValue)
+  })
 })
