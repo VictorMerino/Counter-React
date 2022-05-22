50af821e32ca773612b061cc5390e50c3c7b45f8
Victor Merino <victor.merinogarcia@gmail.com>
2022-05-22 22:09:37 +0200

    Add basic test suite for CounterApp

---
+++ src/tests/CounterApp.test.js
@@ -0,0 +1,14 @@
+import { render, screen } from '@testing-library/react'
+import CounterApp from '../CounterApp'
+
+/**
+ * Counter app should:
+ * show default value
+ * substract
+ * add
+ * rese
+ */
+describe.skip('CounterApp', () => {
+  const defaultValue = 0
+  test('should show message "Hi Apple"', async () => {})
+})
