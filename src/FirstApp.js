29fddcb2c213c69273fc40ebedb2b42bd8338cbb
Victor Merino <victor.merinogarcia@gmail.com>
2022-05-22 11:37:16 +0200

    How to print an object

--- src/FirstApp.js
+++ src/FirstApp.js
@@ -1,8 +1,13 @@
 const FirstApp = () => {
   const userName = 'Mader Faker'
+  const object = {
+    name: 'Name 1',
+    surname: 'Shurmane',
+  }
   return (
     <>
       <h1>Hola {userName}</h1>
+      <pre>{JSON.stringify(object, null, 2)}</pre>
     </>
   )
 }
