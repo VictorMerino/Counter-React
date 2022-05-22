cdb47e1b49a7a3a04dc6cce9966b3d67b26d406e
Victor Merino <victor.merinogarcia@gmail.com>
2022-05-22 20:15:03 +0200

    Add an actual test checking for hero array

--- src/tests/base/09-promises.test.js
+++ src/tests/base/09-promises.test.js
@@ -1,11 +1,12 @@
 import { getHeroByIdAsync } from '../../base/09-promises'
+import { heros } from '../../data/heros'
 
 describe('09-promises', () => {
   test('getHeroByIdAsync shoud return a hero', done => {
     const id = 1
 
     getHeroByIdAsync(id).then(hero => {
-      expect(true).toBe(false)
+      expect(hero).toBe(heros[0])
       done()
     })
   })
